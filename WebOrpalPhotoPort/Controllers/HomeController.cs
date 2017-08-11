dd25ce6f8b8eb3643dfbb5f091935f4e57946125
dmitry741 <dmitrypavlov74@gmail.com>
2017-08-11 14:19:59 +0300

    show fake users
    ridan

--- WebOrpalPhotoPort/Controllers/HomeController.cs
+++ WebOrpalPhotoPort/Controllers/HomeController.cs
@@ -8,10 +8,38 @@ namespace WebOrpalPhotoPort.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        /// <summary>
+        /// View the main page
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
-            return View();
+            var list = new List<Models.User>();
+
+            Models.User user1 = new Models.User
+            {
+                id = 1,
+                Name = "Орк-Правдерез",
+                Email = "dmitrypavlov74@gmail.com",
+                Login = "orc",
+                Password = "12345",
+                Role = 1            
+            };
+
+            Models.User user2 = new Models.User
+            {
+                id = 2,
+                Name = "Паладин Света",
+                Email = "sergey.suloev@gmail.com",
+                Login = "паладин",
+                Password = "12345",
+                Role = 1
+            };
+
+            list.Add(user1);
+            list.Add(user2);
+
+            return View(list);
         }
     }
 }
