88b691ca675cda514789c65520f27a5674024a21
dmitry741 <dmitrypavlov74@gmail.com>
2017-08-29 15:12:34 +0300

    add simple test for Hostring
    ridan

---
+++ UnitTestProjectDataBaseEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectDataBaseEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Danfoss")]
+[assembly: AssemblyProduct("UnitTestProjectDataBaseEngine")]
+[assembly: AssemblyCopyright("Copyright © Danfoss 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4184af7e-81fc-4899-baa8-12b5867771a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectDataBaseEngine/UnitTestDataBaseOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using OrpalPhotoPort.Services.Base;
+using OrpalPhotoPort.Services;
+using OrpalPhotoPort.Domain.Entities;
+using Ninject;
+
+namespace UnitTestProjectDataBaseEngine
+{
+    [TestClass]
+    public class UnitTestDataBaseOperations
+    {
+        IDataBaseEngine GetDataBaseCoClass()
+        {
+            IKernel standartKernel = new StandardKernel();
+            standartKernel.Bind<IDataBaseEngine>().To<DataBaseLiteIssue>();
+
+            return standartKernel.Get<IDataBaseEngine>();
+        }
+
+        [TestMethod]
+        public void AddRemoveOparations()
+        {
+            IDataBaseEngine idbe = GetDataBaseCoClass();
+
+            int curCount = idbe.GetUsers().Count();
+            DateTime now = DateTime.Now;
+            string login = "user" + now.Second + now.Minute + now.Hour + now.Day + now.Month + now.Year;
+
+            User user = new User
+            {
+                Name = "Random user",
+                Email = Guid.NewGuid().ToString().Replace("-", string.Empty) + "@gmail.ru",
+                Login = login,
+                Password = "12345678",
+                Role = 0,
+                RegDateTime = now,
+                ActiveStatus = 0
+            };
+
+            Assert.IsTrue(idbe.AddUser(user));
+
+            User addedUser = idbe.GetUsers().FirstOrDefault(x => x.Login == login);
+
+            Assert.IsNotNull(addedUser);
+
+            Assert.IsTrue(idbe.RemoveUserAt(addedUser.id));
+
+            addedUser = idbe.GetUsers().FirstOrDefault(x => x.Login == login);
+
+            Assert.IsNull(addedUser);
+        }
+    }
+}
